4879600504c2a6e65bff077bb465508ff1994ace
dominhduc <dominhduc12022001@gmail.com>
2025-03-25 10:59:19 +0700

    add id

--- src/main/java/com/dts/jenkinsdemo/api/Controller.java
+++ src/main/java/com/dts/jenkinsdemo/api/Controller.java
@@ -3,6 +3,7 @@ package com.dts.jenkinsdemo.api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,9 @@ public class Controller {
     @GetMapping("/get")
     public ResponseEntity<String> getTest() {
         return ResponseEntity.ok("200 response");
+    }
+    @GetMapping("/get-id")
+    public ResponseEntity<String> getTestId(@RequestParam(required = false) String id) {
+        return ResponseEntity.ok("200 response: " + id);
     }
 }
